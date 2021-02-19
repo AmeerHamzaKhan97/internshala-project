d3a8983767f910459d9e43bd797dab4505e64b5b
AmeerHamzaKhan97 <76786144+AmeerHamzaKhan97@users.noreply.github.com>
2021-02-19 16:06:49 +0530

    Add files via upload

--- intershala-project1/main.js
+++ intershala-project1/main.js
@@ -5,12 +5,26 @@ let barChart = new Chart(myChart,{
     data:{
         labels:['20', ' ','25',' ','30',' ','35','','40','','60','','65'],
         datasets:[{
-            data:[,0,100,200,300,50]
+            data:[0,100,200,300,500]
             
            
         }]
     },
-    options:{}
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    // Include a dollar sign in the ticks
+                    callback: function(value, index, values) {
+                        return '$' + value;
+                    },
+                    max: 300,
+                min: 0,
+                stepSize: 100
+                }
+            }]
+        }
+    }
 });
 
 
